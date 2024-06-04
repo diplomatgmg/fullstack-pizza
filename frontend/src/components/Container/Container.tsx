1c99cd0b58cf18237160be5f9a1f30ae9fd23157
Кирилл Попов <diplomatgmg@gmail.com>
2024-06-04 15:10:13 +0300

    Рефакторинг

--- frontend/src/components/Container/Container.tsx
+++ frontend/src/components/Container/Container.tsx
@@ -1,13 +1,14 @@
 import { FC, type ReactElement } from "react"
 import styled from "styled-components"
 
+interface ContainerProps {
+  children: ReactElement
+}
+
 const ContainerStyle = styled.div`
   padding: 2rem 4rem 0 4rem;
 `
 
-interface ContainerProps {
-  children: ReactElement
-}
 const Container: FC<ContainerProps> = ({ children }): ReactElement => {
   return <ContainerStyle>{children}</ContainerStyle>
 }
