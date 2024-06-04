44015694b8d5aaf60513c100143f8c2025aa6014
Кирилл Попов <diplomatgmg@gmail.com>
2024-06-04 17:53:51 +0300

    Добавлен компонент сортировки

--- frontend/src/components/FilterPanel/FilterPanel.tsx
+++ frontend/src/components/FilterPanel/FilterPanel.tsx
@@ -1,12 +1,20 @@
 import { type ReactElement } from "react"
 import CategoryList from "./CategoryList/CategoryList.tsx"
+import Sort from "./Sort/Sort.tsx"
+import styled from "styled-components"
+
+const FilterPanelStyle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
 
 const FilterPanel = (): ReactElement => {
   return (
-    <div>
+    <FilterPanelStyle>
       <CategoryList />
-      {/*<Sort />*/}
-    </div>
+      <Sort />
+    </FilterPanelStyle>
   )
 }
 
---
+++ frontend/src/components/FilterPanel/Sort/Sort.tsx
@@ -0,0 +1,14 @@
+import { type ReactElement } from "react"
+
+const Sort = (): ReactElement => {
+  return (
+    <div>
+      Сортировка по:{" "}
+      <a href="#">
+        <b>цене</b>
+      </a>
+    </div>
+  )
+}
+
+export default Sort
