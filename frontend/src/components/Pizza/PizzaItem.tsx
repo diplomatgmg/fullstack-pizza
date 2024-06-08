c589f42e0543f091839395bf39df14ba7d24b992
Кирилл Попов <diplomatgmg@gmail.com>
2024-06-08 10:39:31 +0300

    Добавлен блок с пиццей

---
+++ frontend/src/components/Pizza/PizzaItem.tsx
@@ -0,0 +1,76 @@
+import { type FC, type ReactElement } from "react"
+import { TPizza } from "../../types/pizzaTypes.ts"
+import Button from "../Button/Button.tsx"
+import styled from "styled-components"
+import { colors } from "../../styles/theme.ts"
+
+interface PizzaItemProps {
+  pizza: TPizza
+}
+
+const PizzaItemStyle = styled.div`
+  height: 490px;
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: 0 0 10px 10px rgba(0, 0, 0, 0.05);
+  padding: 1rem 2rem;
+  border-radius: 1rem;
+`
+
+const Image = styled.img`
+  margin-top: 1rem;
+  margin-left: 1rem;
+  user-select: none;
+`
+
+const Name = styled.h2`
+  font-size: 1.75rem;
+  margin: 1rem 0;
+  text-align: center;
+`
+
+const Ingredients = styled.p`
+  font-size: 0.9rem;
+  text-align: center;
+`
+
+const Footer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+
+  width: 100%;
+  margin-top: auto;
+`
+
+const Price = styled.p`
+  font-size: 1.25rem;
+  font-weight: bold;
+
+  white-space: nowrap;
+`
+
+const PizzaItem: FC<PizzaItemProps> = ({ pizza }): ReactElement => {
+  return (
+    <PizzaItemStyle>
+      <Image src={pizza.image} alt={pizza.name} />
+      <Name>{pizza.name}</Name>
+      <Ingredients>
+        {pizza.ingredients.map((ingredient) => ingredient.name).join(", ")}
+      </Ingredients>
+      <Footer>
+        <Price>от {pizza.price} ₽</Price>
+        <Button
+          bgColor={colors.white}
+          borderColor={colors.orange}
+          color={colors.orange}>
+          В корзину
+        </Button>
+      </Footer>
+    </PizzaItemStyle>
+  )
+}
+
+export default PizzaItem
---
+++ frontend/src/components/Pizza/PizzaList.tsx
@@ -0,0 +1,30 @@
+import { type ReactElement } from "react"
+import { useGetPizzaQuery } from "../../store/api/pizzaApi.ts"
+import PizzaItem from "./PizzaItem.tsx"
+import styled from "styled-components"
+
+const PizzaListStyle = styled.ul`
+  display: grid;
+
+  justify-items: center;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 30px;
+`
+
+const PizzaList = (): ReactElement => {
+  const { data: pizzas, isError } = useGetPizzaQuery()
+
+  if (isError) {
+    return <h1>error</h1>
+  }
+
+  return (
+    <PizzaListStyle>
+      {pizzas
+        ?.slice(0, 4)
+        .map((pizza) => <PizzaItem key={pizza.id} pizza={pizza} />)}
+    </PizzaListStyle>
+  )
+}
+
+export default PizzaList
