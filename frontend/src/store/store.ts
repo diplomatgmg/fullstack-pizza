c8c55a5a6e8f1f581c83f0f41b3c15f155166677
Кирилл Попов <diplomatgmg@gmail.com>
2024-06-08 15:14:55 +0300

    Добавлен searchParamsReducer

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit"
 import pizzaApi from "./api/pizzaApi.ts"
+import searchParamsReducer from "./slice/searchParamsSlice.ts"
 
 const store = configureStore({
   reducer: {
     [pizzaApi.reducerPath]: pizzaApi.reducer,
+    searchParams: searchParamsReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(pizzaApi.middleware),
 })
 
 export default store
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
