d38cf9ba6403509d7f14cae74b16927ed0b3039d
Кирилл Попов <diplomatgmg@gmail.com>
2024-06-12 15:05:31 +0300

    Реализована фильтрация по умолчанию/цене/названию

--- frontend/src/components/FilterPanel/Sort/Sort.tsx
+++ frontend/src/components/FilterPanel/Sort/Sort.tsx
@@ -1,17 +1,91 @@
-import { type ReactElement } from "react"
+import { type ReactElement, useState } from "react"
 import styled from "styled-components"
 
+import Img from "../../Img/Img.tsx"
+import { colors } from "../../../styles/theme.ts"
+import Popup from "reactjs-popup"
+import "reactjs-popup/dist/index.css"
+import { SORT_FILTER_OPTIONS, TSortFilterOption } from "../../../constants.ts"
+import useAppDispatch from "../../../store/hooks/useAppDispatch.tsx"
+import { setOrdering } from "../../../store/slice/searchParamsSlice.ts"
+
 const SortStyle = styled.div`
+  display: flex;
+  align-items: center;
   white-space: nowrap;
 `
 
+const SpanStyle = styled.span`
+  margin-right: 0.5rem;
+`
+
+const SortLink = styled.a`
+  display: flex;
+  align-items: center;
+  gap: 0.25rem;
+  text-decoration: none;
+  cursor: pointer;
+`
+
+const BoldStyle = styled.b`
+  color: ${colors.orange};
+`
+
+const FilterList = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: white;
+  border: 1px solid ${colors.grey};
+  border-radius: 5px;
+  padding: 1rem;
+`
+
+const FilterItem = styled.a`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  margin-bottom: 0.5rem;
+  text-decoration: none;
+  color: ${colors.black};
+  cursor: pointer;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+`
+
 const Sort = (): ReactElement => {
+  const [selectedFilter, setSelectedFilter] = useState(SORT_FILTER_OPTIONS[0])
+  const dispatch = useAppDispatch()
+
+  const handleSetOrdering = (filter: TSortFilterOption) => {
+    setSelectedFilter(filter)
+    dispatch(setOrdering(filter.ordering))
+  }
+
   return (
     <SortStyle>
-      Сортировка по:{" "}
-      <a href="#">
-        <b>цене</b>
-      </a>
+      <SpanStyle>Сортировка по: </SpanStyle>
+      <Popup
+        trigger={
+          <SortLink>
+            <BoldStyle>{selectedFilter.label}</BoldStyle>
+            <Img src={selectedFilter.icon} />
+          </SortLink>
+        }
+        position="bottom right"
+        on="click">
+        <FilterList>
+          {SORT_FILTER_OPTIONS.map((filter) => (
+            <FilterItem
+              key={filter.ordering}
+              onClick={() => handleSetOrdering(filter)}>
+              <Img src={filter.icon} />
+              {filter.label}
+            </FilterItem>
+          ))}
+        </FilterList>
+      </Popup>
     </SortStyle>
   )
 }
---
+++ frontend/src/constants.ts
@@ -0,0 +1,20 @@
+import AscendingBarsIcon from "/svg/ascending-bars.svg"
+import DefaultSortIcon from "/svg/default-sort.svg"
+import DescendingBarsIcon from "/svg/descending-bars.svg"
+import AscendingAZIcon from "/svg/ascending-a-z.svg"
+import DescendingZAIcon from "/svg/descending-z-a.svg"
+import { TOrderingFields } from "./types/store/searchParamsTypes.ts"
+
+export interface TSortFilterOption {
+  label: string
+  icon: string
+  ordering: TOrderingFields
+}
+
+export const SORT_FILTER_OPTIONS: TSortFilterOption[] = [
+  { label: "умолчанию", icon: DefaultSortIcon, ordering: "" },
+  { label: "цене", icon: AscendingBarsIcon, ordering: "price" },
+  { label: "цене", icon: DescendingBarsIcon, ordering: "-price" },
+  { label: "названию", icon: AscendingAZIcon, ordering: "name" },
+  { label: "названию", icon: DescendingZAIcon, ordering: "-name" },
+]
