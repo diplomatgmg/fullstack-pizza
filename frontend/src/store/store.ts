a87bec1fa567867a0bf09fe8c58b509660964313
Кирилл Попов <diplomatgmg@gmail.com>
2024-06-13 13:38:57 +0300

    Добавлен auth reducer

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,16 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit"
 import pizzaApi from "./api/pizzaApi.ts"
 import searchParamsReducer from "./slice/searchParamsSlice.ts"
+import authReducer from "./slice/authSlice.ts"
+import authApi from "./api/authApi.ts"
 
 const store = configureStore({
   reducer: {
     [pizzaApi.reducerPath]: pizzaApi.reducer,
+    [authApi.reducerPath]: authApi.reducer,
     searchParams: searchParamsReducer,
+    auth: authReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(pizzaApi.middleware),
+    getDefaultMiddleware().concat(pizzaApi.middleware, authApi.middleware),
 })
 
-export default store
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
+
+export default store
