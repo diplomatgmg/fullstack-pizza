51a7d96a3d24af21ff60a1ec1f1e8fa57df0c5ea
Кирилл Попов <diplomatgmg@gmail.com>
2024-06-13 13:18:30 +0300

    Определен process.env для окружения

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    host: "0.0.0.0",
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "")
+  return {
+    define: {
+      "process.env": env,
+    },
+    plugins: [react()],
+    server: {
+      port: 3000,
+      host: "0.0.0.0",
+    },
+  }
 })
