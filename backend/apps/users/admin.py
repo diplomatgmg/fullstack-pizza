b2a0f9bc7b585ef63dbf722df2c4dfa2c3f1e031
Кирилл Попов <diplomatgmg@gmail.com>
2024-06-13 19:59:51 +0300

    Изменена базовая модель пользователя

--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        if not change and obj.password:
+            obj.set_password(obj.password)
+        super().save_model(request, obj, form, change)
--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,3 +1,45 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError(_("The Email field must be set"))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(_("email address"), unique=True)
+    is_staff = models.BooleanField(_("staff status"), default=False)
+    is_superuser = models.BooleanField(_("superuser status"), default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
