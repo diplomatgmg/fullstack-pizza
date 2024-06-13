503a5551766e5332fa73495c90fae5fa77346a66
Кирилл Попов <diplomatgmg@gmail.com>
2024-06-13 21:22:07 +0300

    Обновлена типизация

--- frontend/src/store/api/authApi.ts
+++ frontend/src/store/api/authApi.ts
@@ -1,18 +1,20 @@
 import baseQuery from "./baseApi.ts"
 import { createApi } from "@reduxjs/toolkit/query/react"
+import { TLoginResponse } from "../../types/api/responseTypes.ts"
+import { TLoginRequest } from "../../types/api/requestTypes.ts"
 
 const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: baseQuery,
   endpoints: (builder) => ({
-    login: builder.mutation({
+    login: builder.mutation<TLoginResponse, TLoginRequest>({
       query: (credentials) => ({
         url: "token/",
         method: "POST",
         body: credentials,
       }),
     }),
-    refresh: builder.mutation({
+    refresh: builder.mutation<unknown, unknown>({
       query: (refreshToken) => ({
         url: "token/refresh/",
         method: "POST",
