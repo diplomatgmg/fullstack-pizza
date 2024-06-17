f594cdd035c4d956ed4cd2c709a12296e99e8ae1
Кирилл Попов <diplomatgmg@gmail.com>
2024-06-17 21:03:00 +0300

    Добавлен urls.py для работы с пользователями

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -14,18 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("apps.pizza.urls")),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/", include("apps.users.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
 
