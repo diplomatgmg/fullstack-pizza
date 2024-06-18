35f282ce2844e47ba9eb4827ad4c8c26e61490e6
Кирилл Попов <diplomatgmg@gmail.com>
2024-06-18 22:03:29 +0300

    Обновлена модель пользователя

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -11,10 +11,12 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError(_("The Email field must be set"))
+
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        user.save(using=self._db)
+        user.save()
+
         return user
 
     def create_superuser(self, email, password=None, **extra_fields):
@@ -30,8 +32,13 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(_("email address"), unique=True)
-    is_staff = models.BooleanField(_("staff status"), default=False)
+    email = models.EmailField(db_index=True, unique=True)
+    is_active = models.BooleanField(default=True)
+
+    is_staff = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     objects = UserManager()
 
