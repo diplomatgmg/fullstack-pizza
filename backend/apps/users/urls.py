67d4bd24be1434692d805dbb0b662dc951ef2a40
Кирилл Попов <diplomatgmg@gmail.com>
2024-06-19 17:23:02 +0300

    Добавлен эндпоинт api/health-check/

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,9 +1,10 @@
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.urls import path
-from .views import UserCreateAPIView
+from .views import UserCreateAPIView, HealthCheckAPIView
 
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token-obtain-pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
     path("register/", UserCreateAPIView.as_view(), name="user-register"),
+    path("health-check/", HealthCheckAPIView.as_view(), name="health-check"),
 ]
--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,7 +1,13 @@
 from rest_framework.generics import CreateAPIView
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .serializers import UserSerializer
 
 
 class UserCreateAPIView(CreateAPIView):
     serializer_class = UserSerializer
+
+
+class HealthCheckAPIView(APIView):
+    def get(self, request):
+        return Response({"status": "ok"})
