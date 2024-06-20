6752cd7d6a894be93570000b677867ff6bd061eb
Кирилл Попов <diplomatgmg@gmail.com>
2024-06-20 17:02:16 +0300

    Изменен регион и врем. зона

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -142,9 +142,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ru"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Moscow"
 
 USE_I18N = True
 
