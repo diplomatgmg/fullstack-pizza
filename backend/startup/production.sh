88cc6a22d1595ecdcbbb1c03274ecd6eecafa8f5
Кирилл Попов <diplomatgmg@gmail.com>
2024-06-21 23:29:53 +0300

    Обновлены параметры запуска

--- backend/startup/production.sh
+++ backend/startup/production.sh
@@ -2,9 +2,14 @@ echo "Running default startup..."
 bash "$(pwd)/startup/base.sh"
 echo "Default startup executed"
 
+echo "Collecting static files..."
+python manage.py collectstatic --no-input
+echo "Static files collected"
+
+
+echo "Checking deployment readiness..."
+python manage.py check --deploy
+echo "Deployment readiness checked"
 
 echo "Starting server..."
-python manage.py collectstatic --no-input
-python manage.py check --deploy
 gunicorn core.wsgi
-echo "Create migrations..."
