d949c85f127a7bbbae7d9aaba61fb6b0cc37e1af
Кирилл Попов <diplomatgmg@gmail.com>
2024-06-24 13:39:43 +0300

    Убран media urls

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -140,13 +140,9 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "..", "static"),
-    os.path.join(BASE_DIR, "..", "media"),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "..", "staticfiles")
 
-
-MEDIA_URL = "/static/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "..", "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
