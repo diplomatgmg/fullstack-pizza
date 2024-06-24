fa502dfbbb6f99775c9f30a8ffcc3eedf07822b4
Кирилл Попов <diplomatgmg@gmail.com>
2024-06-24 13:09:38 +0300

    Изменено расположение media

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -145,8 +145,8 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, "..", "staticfiles")
 
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "..", "media")
+MEDIA_URL = "/static/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "..", "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
