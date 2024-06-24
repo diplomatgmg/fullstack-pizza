6854068eb238870887a588beaecaeb8e4375cf8b
Кирилл Попов <diplomatgmg@gmail.com>
2024-06-24 12:22:32 +0300

    Добавлены настройки HSTS и улучшены заголовки безопасности

--- backend/core/settings/production.py
+++ backend/core/settings/production.py
@@ -4,11 +4,25 @@ from .base import *
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "fullstack-pizza.up.railway.app",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://fullstack-pizza.vercel.app",
+]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://*.up.railway.app",
+    "https://fullstack-pizza.vercel.app",
 ]
 
 # whitenoise должен находиться после django.middleware.security.SecurityMiddleware
 MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
+
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_SECONDS = 3600
+
+CSRF_COOKIE_DOMAIN = "https://fullstack-pizza.up.railway.app/"
+CSRF_COOKIE_SECURE = True
+
+SESSION_COOKIE_SECURE = True
