6e0b0ffae9dc608d59e77d2d053ac01825d882c8
Кирилл Попов <diplomatgmg@gmail.com>
2024-06-24 13:02:17 +0300

    Изменено расположение static/media

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -139,9 +139,10 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "..", "static"),
+    os.path.join(BASE_DIR, "..", "media"),
 ]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "staticfiles")
 
 
 MEDIA_URL = "/media/"
